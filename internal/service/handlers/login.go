c93ed0e82835ffcd052179010a0f7ec8cfbcbfec
mark.cherepovskyi <mark.cherepovskyi@gmail.com>
2022-11-24 12:50:03 +0200

    added login endpoint

---
+++ internal/service/handlers/login.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"net/http"
+
+	"gitlab.com/distributed_lab/ape"
+	"gitlab.com/distributed_lab/ape/problems"
+	"gitlab.com/tokene/nonce-auth-svc/internal/service/errors/apierrors"
+	"gitlab.com/tokene/nonce-auth-svc/internal/service/helpers"
+	"gitlab.com/tokene/nonce-auth-svc/internal/service/requests"
+)
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	logger := helpers.Log(r)
+	request, err := requests.NewLoginRequest(r)
+	if err != nil {
+		logger.WithError(err).Debug("bad request")
+		ape.RenderErr(w, apierrors.BadRequest(apierrors.CodeBadRequestData, err))
+		return
+	}
+
+	ethAddress := request.Data.Attributes.AuthPair.Address
+	signature := request.Data.Attributes.AuthPair.SignedMessage
+
+	apiErr, err := helpers.ValidateNonce(ethAddress, signature, r)
+	if err != nil {
+		logger.WithError(err).Debug("failed to validate nonce")
+		ape.RenderErr(w, apiErr)
+		return
+	}
+
+	// success logic
+	doorman := helpers.DoormanConnector(r)
+	pair, err := doorman.GenerateJwtPair(ethAddress, "session")
+	if err != nil {
+		logger.WithError(err).Error("failed to generate jwt")
+		ape.RenderErr(w, problems.InternalError())
+		return
+	}
+
+	ape.Render(w, pair)
+}
--- internal/service/router.go
+++ internal/service/router.go
@@ -28,8 +28,8 @@ func (s *service) router(cfg config.Config) chi.Router {
 	r.Route("/integrations/nonce-auth-svc", func(r chi.Router) {
 		r.Post("/nonce", handlers.GetNonce)
 		r.Get("/refresh_token", handlers.RefreshToken)
+		r.Post("/login", handlers.Login)
 		r.Post("/admin_login", handlers.AdminLogin)
-
 	})
 
 	return r
